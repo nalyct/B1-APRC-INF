79ae2bc783eddc68fd6ae2848e5c997ee17f820a
nalyct <caplet.vinaly@gmail.com>
2025-01-12 21:38:14 +0100

    autoconfig

--- autoconfig.sh
+++ autoconfig.sh
@@ -94,27 +94,3 @@ log "INFO" "Le script d'autoconfiguration s'est correctement déroulé"
 
 ```
 
-Résultat : 
-
-```
-[abc@music ~]$ sudo /opt/autoconfig.sh abc
-21:31:49 [INFO] Le script d'autoconfiguration a démarré
-21:31:49 [INFO] Le script a bien été lancé en root
-21:31:49 [INFO] SELinux est déjà en mode permissive (temporaire)
-21:31:49 [INFO] Le fichier de configuration SELinux est déjà correct
-21:31:49 [INFO] Service de firewalling firewalld est activé
-21:31:49 [INFO] Le service SSH n'utilise pas le port 22. Configuration actuelle correcte
-21:31:50 [INFO] Le nom de la machine est déjà configuré : music
-21:31:50 [ERROR] L'utilisateur it4 n'existe pas !
-[abc@music ~]$ sudo nano /opt/autoconfig.sh
-[abc@music ~]$ sudo nano /opt/autoconfig.sh
-[abc@music ~]$ sudo /opt/autoconfig.sh abc
-21:32:58 [INFO] Le script d'autoconfiguration a démarré
-21:32:58 [INFO] Le script a bien été lancé en root
-21:32:58 [INFO] SELinux est déjà en mode permissive (temporaire)
-21:32:58 [INFO] Le fichier de configuration SELinux est déjà correct
-21:32:58 [INFO] Service de firewalling firewalld est activé
-21:32:58 [INFO] Le service SSH n'utilise pas le port 22. Configuration actuelle correcte
-21:32:59 [INFO] Le nom de la machine est déjà configuré : music
-21:32:59 [INFO] L'utilisateur abc appartient déjà au groupe wheel
-```
